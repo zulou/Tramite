d8a30761944e2962588864fc6ab3caf324bf6a10
root <root@localhost.localdomain>
2019-01-24 21:30:32 -0500

    add admin

--- Core/admin.py
+++ Core/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
-class scoopAdmin(admin.ModelAdmin):
-    search_fields = ['id_detalle_actividad', 'horometro', 'numero_cucharas','material','toneladas']
-    list_display = ('id_detalle_actividad', 'horometro', 'numero_cucharas','material','toneladas')
+class OfficeAdmin(admin.ModelAdmin):
+    search_fields = ['ofi_des']
+    list_display = ('ofi_des')
 
-#admin.site.register(models.operador,operadorAdmin)
+class TupaAdmin(admin.ModelAdmin):
+    search_fields = ['id_ofi_begin', 'id_ofi_end', 'tup_des','tup_requeriments','tup_cost','tup_days']
+    list_display = ('id_ofi_begin', 'id_ofi_end', 'tup_des','tup_requeriments','tup_cost','tup_days')
+
+class Type_documentAdmin(admin.ModelAdmin):
+    search_fields = ['doc_des']
+    list_display = ('doc_des')
+
+class AttachmentsAdmin(admin.ModelAdmin):
+    search_fields = ['att_path']
+    list_display = ('att_path')
+
+class MovementsAdmin(admin.ModelAdmin):
+    search_fields = ['mov_order','id_attachments','id_ofi_begin','id_ofi_end','id_doc_sender','move_recibed']
+    list_display = ('mov_order','id_attachments','id_ofi_begin','id_ofi_end','id_doc_sender','move_recibed')
+
+class Document_identityAdmin(admin.ModelAdmin):
+    search_fields = ['doc_des']
+    list_display = ('doc_des')
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ['id_doc','per_name','per_lastname','per_doc','per_address','per_cellphone','per_type']
+    list_display = ('id_doc','per_name','per_lastname','per_doc','per_address','per_cellphone','per_type')
+
+class DocumentAdmin(admin.ModelAdmin):
+    search_fields = ['id_type_document','id_person','id_tupa','doc_number','doc_exp_number','doc_des','doc_pages','doc_type']
+    list_display = ('id_type_document','id_person','id_tupa','doc_number','doc_exp_number','doc_des','doc_pages','doc_type')
+
+
+
+admin.site.register(models.Office,OfficeAdmin)
+admin.site.register(models.Tupa,TupaAdmin)
+admin.site.register(models.Type_document,Type_documentAdmin)
+admin.site.register(models.Attachments,AttachmentsAdmin)
+admin.site.register(models.Movements,MovementsAdmin)
+admin.site.register(models.Document_identity,Document_identityAdmin)
+admin.site.register(models.Person,PersonAdmin)
+admin.site.register(models.Document,DocumentAdmin)
